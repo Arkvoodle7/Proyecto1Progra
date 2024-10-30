740d0daa1315d5ab3355f8977622158a09c09538
Arkvoodle7 <alexlego100@outlook.com>
2024-10-30 14:07:14 -0600

    Incorporar cambios específicos de archivos desde Upgrade-EntornoBancario-V2

--- bancario/App/src/interfaces/ICuenta.java
+++ bancario/App/src/interfaces/ICuenta.java
@@ -4,6 +4,8 @@ import entities.Cuenta;
 
 public interface ICuenta {
     Cuenta buscarCuenta(String identificacion, String numeroCuenta);
+
+    String obtenerNombreUsuario(String identificacion);
     void actualizarCuenta(Cuenta cuenta);
     boolean clienteExiste(String identificacion);  // Agregar esta línea
 }
