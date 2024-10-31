a79990ca94fa4192fae6373ee5eb1dc9a3f39ba4
Arkvoodle7 <alexlego100@outlook.com>
2024-10-30 23:44:29 -0600

    Reagregar carpeta WSUsuarios desde la rama Skeleton-Gradle-Spring

---
+++ WSUsuarios/src/main/java/com/pagosmoviles/Main.java
@@ -0,0 +1,7 @@
+package com.pagosmoviles;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" * WebService Soap ejecutándose en el puerto 8080");
+    }
+}
