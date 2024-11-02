6bcfb3962bf29b7705bfe16adacaa7bf538a9776
Arkvoodle7 <alexlego100@outlook.com>
2024-11-02 17:17:49 -0600

    Reemplazar la carpeta WSUsuarios con la versión de PruebaDanielCambiosBancario

--- WSUsuarios/src/main/java/com/pagosmoviles/Main.java
+++
@@ -1,7 +0,0 @@
-package com.pagosmoviles;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(" * WebService Soap ejecutándose en el puerto 8080");
-    }
-}
