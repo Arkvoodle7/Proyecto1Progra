315d567c4fdc84c8445b08442676b4883da5f26f
Arkvoodle7 <alexlego100@outlook.com>
2024-11-03 14:12:04 -0600

    Intercambiar nombres de carpetas WSU1 y WSU2

--- WSUsuarios/WSU1/src/main/java/com/pagosmoviles/Main.java
+++ WSUsuarios/WSU1/src/main/java/com/pagosmoviles/Main.java
@@ -14,21 +14,11 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @Bean(name = "usuariosDataSource")
-    public DataSource usuariosDataSource() {
+    @Bean
+    public DataSource dataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         dataSourceBuilder.url("jdbc:sqlserver://DESKTOP-1U7DCG4;databaseName=PagosMovilesUsuarios;encrypt=true;trustServerCertificate=true");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("root");
-        return dataSourceBuilder.build();
-    }
-
-    @Bean(name = "bancoDataSource")
-    public DataSource bancoDataSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSourceBuilder.url("jdbc:sqlserver://DESKTOP-1U7DCG4;databaseName=PagosMovilesBancario;encrypt=true;trustServerCertificate=true");
         dataSourceBuilder.username("sa");
         dataSourceBuilder.password("root");
         return dataSourceBuilder.build();
--- WSUsuarios/WSU1/src/main/java/com/pagosmoviles/repository/UsuarioRepository.java
+++ WSUsuarios/WSU1/src/main/java/com/pagosmoviles/repository/UsuarioRepository.java
@@ -2,10 +2,8 @@ package com.pagosmoviles.repository;
 
 import com.pagosmoviles.entities.Usuario;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
-
 import java.sql.ResultSet;
 
 @Repository
@@ -13,7 +11,7 @@ public class UsuarioRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public UsuarioRepository(@Qualifier("usuariosJdbcTemplate") JdbcTemplate jdbcTemplate) {
+    public UsuarioRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -31,34 +29,7 @@ public class UsuarioRepository {
                 return usuario;
             });
         } catch (EmptyResultDataAccessException e) {
-            return null;
-        }
-    }
-
-    public void crearUsuario(Usuario usuario) {
-        String query = "INSERT INTO Usuarios (identificacion, nombre_usuario, nombre_completo, contrasena, telefono) VALUES (?, ?, ?, ?, ?)";
-        jdbcTemplate.update(query, usuario.getIdentificacion(), usuario.getNombreUsuario(), usuario.getNombreCompleto(), usuario.getContrasena(), usuario.getTelefono());
-    }
-
-    public void editarUsuario(Usuario usuario) {
-        String query = "UPDATE Usuarios SET nombre_usuario = ?, nombre_completo = ?, contrasena = ?, telefono = ? WHERE identificacion = ?";
-        jdbcTemplate.update(query, usuario.getNombreUsuario(), usuario.getNombreCompleto(), usuario.getContrasena(), usuario.getTelefono(), usuario.getIdentificacion());
-    }
-
-    public Usuario obtenerUsuarioPorIdentificacion(String identificacion) {
-        String query = "SELECT * FROM Usuarios WHERE identificacion = ?";
-
-        try {
-            return jdbcTemplate.queryForObject(query, new Object[]{identificacion}, (ResultSet rs, int rowNum) -> {
-                Usuario usuario = new Usuario();
-                usuario.setIdentificacion(rs.getString("identificacion"));
-                usuario.setNombreUsuario(rs.getString("nombre_usuario"));
-                usuario.setNombreCompleto(rs.getString("nombre_completo"));
-                usuario.setContrasena(rs.getString("contrasena"));
-                usuario.setTelefono(rs.getString("telefono"));
-                return usuario;
-            });
-        } catch (EmptyResultDataAccessException e) {
+            // No se encontró el usuario, devolver null
             return null;
         }
     }
