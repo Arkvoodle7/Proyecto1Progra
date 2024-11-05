20f9c7a1239a8b639305bc23cde9201792ab8151
Arkvoodle7 <alexlego100@outlook.com>
2024-11-05 17:49:38 -0600

    Importar carpetas WSU5 y WSU6 desde la rama DorianWS

---
+++ WSUsuarios/WSU6/src/main/java/com/pagosmoviles/dto/AuthRequestDto.java
@@ -0,0 +1,34 @@
+package com.pagosmoviles.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+@JacksonXmlRootElement(localName = "authRequestDto")
+public class AuthRequestDto {
+
+    @JacksonXmlProperty(localName = "nombreUsuario")
+    private String nombreUsuario;
+
+    @JacksonXmlProperty(localName = "contrasena")
+    private String contrasena;
+
+    // Constructor por defecto necesario para la deserialización
+    public AuthRequestDto() {
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
