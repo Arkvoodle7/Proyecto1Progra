3ef4aacbe8f199ed75b47273028ae178e2e1e17d
doriancm05 <dorian.campos05@gmail.com>
2024-11-05 00:43:12 -0600

    WSU5 Y WSU6 Funcionales

--- WSUsuarios/WSU5/src/main/java/com/pagosmoviles/Main.java
+++ WSUsuarios/WSU5/src/main/java/com/pagosmoviles/Main.java
@@ -2,25 +2,11 @@ package com.pagosmoviles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import javax.sql.DataSource;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSourceBuilder.url("jdbc:sqlserver://DESKTOP-1U7DCG4;databaseName=PagosMovilesUsuarios;encrypt=true;trustServerCertificate=true");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("root");
-        return dataSourceBuilder.build();
     }
 }
