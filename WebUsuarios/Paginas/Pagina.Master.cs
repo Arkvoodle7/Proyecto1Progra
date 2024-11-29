53ee8e50287e82dd7bdf7d9e998f13b0cc0cd320
Arkvoodle7 <alexlego100@outlook.com>
2024-11-29 14:49:21 -0600

    Copiar WebAdministracion y WebUsuarios desde RamaDanielFinalPC a RamaDanielFinalLaptop

---
+++ WebUsuarios/Paginas/Pagina.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebUsuarios.Paginas
+{
+    public partial class Pagina : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Usuario"] != null) //usuario autenticado
+            {
+                OpcionesLogin.Visible = false;
+                OpcionesUsuario.Visible = true;
+            }
+            else //no autenticado
+            {
+                OpcionesLogin.Visible = true;
+                OpcionesUsuario.Visible = false;
+            }
+        }
+
+    }
+}
