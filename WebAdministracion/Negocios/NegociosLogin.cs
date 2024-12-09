5ef00048b904c9bab8909cf36e140d9ac4e1204e
Daniel Cordero <81249972+Arkvoodle7@users.noreply.github.com>
2024-12-09 12:09:34 -0600

    Update NegociosLogin.cs

--- WebAdministracion/Negocios/NegociosLogin.cs
+++ WebAdministracion/Negocios/NegociosLogin.cs
@@ -1,12 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Negocios.Login_Reference1;
 
 namespace Negocios
 {
-    internal class NegociosLogin
+    public class NegociosLogin
     {
+        public LoginAdmin ValidarUsuario(string usuario, string contraseña)
+        {
+            try
+            {
+                var servicio = new Login_Reference1.WSA1SoapClient("WSA1Soap");
+                var resultado = servicio.Login_Administradores(usuario, contraseña);
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                //Mensaje de error
+                throw new Exception($"Error al consumir el servicio web: {ex.Message}", ex);
+            }
+        }
     }
 }
