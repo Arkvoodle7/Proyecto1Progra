2229e90c033a023a1a06b6221abe64c420691f3f
Arkvoodle7 <alexlego100@outlook.com>
2024-12-09 12:05:01 -0600

    Agregar Modelos

---
+++ WebAdministracion/Modelos/ModeloAdmins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class ModeloAdmins
+    {
+        public string NombreUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
