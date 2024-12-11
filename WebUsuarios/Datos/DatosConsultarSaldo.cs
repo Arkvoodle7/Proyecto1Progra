5966bd496a8c4bcac9b83014b5194a8f61578a02
Daniel Cordero <81249972+Arkvoodle7@users.noreply.github.com>
2024-12-11 12:56:34 -0600

    Update DatosConsultarSaldo.cs

--- WebUsuarios/Datos/DatosConsultarSaldo.cs
+++ WebUsuarios/Datos/DatosConsultarSaldo.cs
@@ -1,12 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datos
 {
-    internal class DatosConsultarSaldo
+    public class DatosConsultarSaldo
     {
+        private readonly string _soapSaldoEndpoint = "http://localhost:8084/wsusuarios";
+
+        public async Task<string> ConsultarSaldo(string telefono)
+        {
+            var soapSaldoRequest = $@"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:wsu='http://pagosmoviles.com/wsusuarios'>
+                   <soapenv:Header/>
+                   <soapenv:Body>
+                      <wsu:saldoRequest>
+                         <wsu:telefono>{telefono}</wsu:telefono>
+                      </wsu:saldoRequest>
+                   </soapenv:Body>
+                </soapenv:Envelope>";
+
+            using (var client = new HttpClient())
+            {
+                var saldoContent = new StringContent(soapSaldoRequest, Encoding.UTF8, "text/xml");
+                var saldoResponse = await client.PostAsync(_soapSaldoEndpoint, saldoContent);
+                return await saldoResponse.Content.ReadAsStringAsync();
+
+            }
+        }
     }
 }
