11f98da9c98925b8de4d714f3abc963c46a80373
Daniel Cordero <81249972+Arkvoodle7@users.noreply.github.com>
2024-12-13 10:24:59 -0600

    Update NegociosTransferencias.cs

--- WebUsuarios/Negocios/NegociosTransferencias.cs
+++ WebUsuarios/Negocios/NegociosTransferencias.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
+using Datos;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Negocios
 {
-    internal class NegociosTransferencias
+    public class NegociosTransferencias
     {
+        private readonly DatosTransferencias _datosTransferencias;
+
+        public NegociosTransferencias()
+        {
+            _datosTransferencias = new DatosTransferencias();
+        }
+
+        public async Task<(bool isSuccess, string mensaje)> RealizarTransferencia(string telefonoDestino, decimal monto, string descripcion)
+        {
+            try
+            {
+                var response = await _datosTransferencias.RealizarTransferencia(telefonoDestino, monto, descripcion);
+
+                var xml = XDocument.Parse(response);
+                var codigo = xml.Descendants().FirstOrDefault(e => e.Name.LocalName == "codigo")?.Value;
+                var descripcionRespuesta = xml.Descendants().FirstOrDefault(e => e.Name.LocalName == "descripcion")?.Value;
+
+                if (codigo == "0")
+                {
+                    return (true, descripcionRespuesta ?? "Transacción realizada con éxito.");
+                }
+                else
+                {
+                    return (false, descripcionRespuesta ?? "Ocurrió un error en la transacción.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error en NegociosTransferencias: {ex.Message}");
+                return (false, "No se pudo procesar la transacción. Intentelo nuevamente en unos minutos.");
+            }
+        }
     }
 }
